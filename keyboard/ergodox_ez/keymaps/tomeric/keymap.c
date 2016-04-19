884fc76e023e694bc9cce8e187eeef193074fefd
Tom-Eric Gerritsen <ik@tom-eric.info>
2016-04-19 20:46:24 +0200

    Bugfixes and added stenography layer

--- keyboard/ergodox_ez/keymaps/tomeric/keymap.c
+++ keyboard/ergodox_ez/keymaps/tomeric/keymap.c
@@ -8,6 +8,7 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define PLVR 3 // Plover layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -18,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L2  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | '"/L2  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  Enter |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Hyper|           |  L3  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LGui | Left |  Up  | Down | Right|                                       |Grv/L1|  '"  |   [  |   ]  | RGui |
@@ -44,10 +45,10 @@ LT(MDIA,KC_QUOT), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
                                                               KC_VOLU,
                                              KC_SPC,  KC_TAB, KC_VOLD,
         // right hand
-             KC_MINQ,     KC_6,   KC_7,           KC_8,   KC_9,   KC_0,             KC_BSPC,
+             KC_MINS,     KC_6,   KC_7,           KC_8,   KC_9,   KC_0,             KC_BSPC,
              TG(2),       KC_Y,   KC_U,           KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-             MEH_T(KC_NO),KC_N,   KC_M,           KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             TG(3),       KC_N,   KC_M,           KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   LT(SYMB,KC_GRV),KC_QUOT,KC_LBRC,KC_RBRC,          KC_RGUI,
              KC_LBRC,KC_RBRC,
              KC_PGUP,
@@ -117,7 +118,7 @@ LT(MDIA,KC_QUOT), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
  *                                 `--------------------'       `--------------------'
  */
 // MEDIA AND MOUSE
-KEYMAP(
+[MDIA] = KEYMAP(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -135,6 +136,49 @@ KEYMAP(
        KC_MPRV, KC_MNXT,
        KC_VOLU,
        KC_VOLD, KC_MPLY, KC_WBAK
+),
+
+/* Keymap 3: Steno for Plover from https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |   8  |   9  |  0   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   q  |   w  |   e  |   r  |   t  |------|           |------|  y   |  u   |   i  |   o  |  p   |   [    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   a  |   s  |   d  |   f  |   g  |      |           |      |  h   |  j   |   k  |   l  |  ;   |   '    |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |   c  |   v  |------|       |------|  n   |  m   |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+
+[PLVR] = KEYMAP(  // layout: layer 3: Steno for Plover
+        // left hand
+        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_NO, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
+        KC_NO, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
+        KC_NO, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_NO,
+        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                                       KC_FN4, KC_NO,
+                                               KC_NO,
+                               KC_C,   KC_V,   KC_NO,
+        // right hand
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,
+        KC_TRNS,  KC_6,    KC_7,    KC_8,    KC_9,  KC_0,    KC_NO,
+                  KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,    KC_LBRC,
+        KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+                           KC_TRNS, KC_TRNS, KC_NO, KC_NO,   KC_NO,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS,
+        KC_NO,  KC_N,   KC_M
 ),
 };
 
@@ -164,20 +208,23 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
+    
     uint8_t layer = biton32(layer_state);
-
+    
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
+        case SYMB:
             ergodox_right_led_1_on();
             break;
-        case 2:
+        case MDIA:
             ergodox_right_led_2_on();
+            break;
+        case PLVR:
+            ergodox_right_led_3_on();
             break;
         default:
             // none
