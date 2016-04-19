85a5ea6d1851708be8b056ac3193d032f2b5e5a3
Tom-Eric Gerritsen <ik@tom-eric.info>
2016-04-19 20:25:57 +0200

    Switch positions of '-' and '=' in top row, switch positions of Space and Enter under right thumb, add more keys to symbol layer.

--- keyboard/ergodox_ez/keymaps/tomeric/keymap.c
+++ keyboard/ergodox_ez/keymaps/tomeric/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |  -   |           |   =  |   6  |   7  |   8  |   9  |   0  |  BkSp  |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |  =   |           |   -  |   6  |   7  |   8  |   9  |   0  |  BkSp  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L2  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                       |Ctr/Esc|  Alt |       |   [  |   ]  |
  *                                ,------|-------|------|       |------+--------+------.
  *                                |      |       |VolUp |       | PgUp |        |      |
- *                                | Space|  Tab  |------|       |------|  Space |Enter |
+ *                                | Space|  Tab  |------|       |------|  Enter | Space|
  *                                |      |       |VolDwn|       | PgDn |        |      |
  *                                `---------------------'       `----------------------'
  */
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,
+        KC_ESC,   KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
         KC_TAB,   KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
 LT(MDIA,KC_QUOT), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,  CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
@@ -44,25 +44,25 @@ LT(MDIA,KC_QUOT), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
                                                               KC_VOLU,
                                              KC_SPC,  KC_TAB, KC_VOLD,
         // right hand
-             KC_EQL,      KC_6,   KC_7,           KC_8,   KC_9,   KC_0,             KC_BSPC,
+             KC_MINQ,     KC_6,   KC_7,           KC_8,   KC_9,   KC_0,             KC_BSPC,
              TG(2),       KC_Y,   KC_U,           KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
              MEH_T(KC_NO),KC_N,   KC_M,           KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   LT(SYMB,KC_GRV),KC_QUOT,KC_LBRC,KC_RBRC,          KC_RGUI,
              KC_LBRC,KC_RBRC,
              KC_PGUP,
-             KC_PGDN,KC_SPC, KC_ENT
+             KC_PGDN,KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |   -    |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
+ * |   +    |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |   =    |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -78,9 +78,9 @@ LT(MDIA,KC_QUOT), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_MINS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_PLUS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_EQL, KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
